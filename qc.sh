ea6240e44cba0bc056ad8b26393488e7e72484be
Skydark Chen <skydark2@gmail.com>
2012-10-21 16:06:42 +0800

    add bash completion

--- qc.sh
+++ qc.sh
@@ -17,3 +17,40 @@ function c() {
             ;;
     esac
 }
+
+function _c() {
+    local cur prev opts bookmarks
+    COMPREPLY=()
+    bookmarks=""
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="-a --add -c --save_current -d --delete -l --list -L --list_all -h --help"
+
+    case "${prev}" in
+        -a|--add)
+            bookmarks="$(c --bash_completion add)"
+            ;;
+        -l|--list|-L|--list_all|-c|--save_current|-h|--help)
+            ;;
+        -d|--delete)
+            bookmarks="$(c --bash_completion bookmark)"
+            ;;
+        *)
+            if [[ ${cur:0:1} == "-" ]]; then
+                COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+                return 0
+            else
+                local test="${COMP_WORDS[COMP_CWORD-1]}"
+                if [ x"$test" == x"-a" ] || [ x"$test" == x"--add" ] ; then
+                    bookmarks="$(c --bash_completion path)"
+                else
+                    bookmarks="$(c --bash_completion all)"
+                fi
+            fi
+            ;;
+    esac
+    COMPREPLY=( $(compgen -W "${bookmarks}" -- ${cur}) )
+}
+
+complete -o nospace -F _c c
+complete -o nospace -F _c qc
